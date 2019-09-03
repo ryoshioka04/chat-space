8d8027e8bb10acd344b2898c498c472cd3bece40
Ryoma Yoshioka <r.yoshioka4@gmail.com>
2019-09-04 01:08:50 +0900

    user-management-implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!;
+  before_action :configure_permitted_parameters, if: :devise_controller?;
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  # def after_sign_in_path(resource)
+  #   index_root_path
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   new_user_session_path
+  # end
 end
